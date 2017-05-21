996f9b84060fb6697f5d7ee197e7a5d121066aa0
Cytren <contact@cytren.com>
2017-05-21 19:42:07 +0100

    Added response inject

---
+++ src/main/inject/response.ts
@@ -0,0 +1,64 @@
+
+export interface Response {
+    readonly object: any;
+}
+
+export abstract class BasicResponse implements Response {
+
+    private _object: any;
+
+    public get object(): any {
+        if (!this._object) { return { error: 'The endpoint failed to respond.' }; }
+        return this._object;
+    }
+
+    protected set response(value: any) {
+        if (this._object) { throw new Error('The response value has already been set.'); }
+        this._object = value;
+    }
+
+    public error(message: string) {
+        this._object = { error: message };
+    }
+}
+
+export class IntegerResponse extends BasicResponse {
+
+    public value(value: number) {
+        if (value % 1 !== 0) { throw new Error('The value must be an integer.'); }
+        this.response = { integer: value };
+    }
+}
+
+export class FloatResponse extends BasicResponse {
+
+    public value(value: number) {
+        this.response = { float: value };
+    }
+}
+
+export class StringResponse extends BasicResponse {
+
+    public value(value: string) {
+        this.response = { string: value };
+    }
+}
+
+export class BooleanResponse extends BasicResponse {
+
+    public value(value: boolean) {
+        this.response = { boolean: value };
+    }
+}
+
+export class SuccessResponse extends BasicResponse {
+
+    public success(message?: string) {
+
+        if (message) {
+            this.response = { success: message };
+        } else {
+            this.response = { success: "The endpoint reported success." };
+        }
+    }
+}
