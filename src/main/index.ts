8d7c3eb307adf4dcbd8a8860cbfa66ec6c065b12
Cytren <contact@cytren.com>
2017-05-21 01:51:02 +0100

    Moved index writing to separate module

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,14 +1,11 @@
 
-import * as fs from 'fs';
-import * as del from 'del';
+import * as fs from "fs";
+import * as del from "del";
 import * as apiscript from "apiscript";
-import * as transform from "./util/text-transformers";
 
-import {TypescriptWriter} from "./writer/typescript-writer";
-import {writeRequestClasses} from "./writer/request-writer";
-import {writeResponseClasses} from "./writer/response-writer";
 import {writeEntityClasses} from "./writer/entity-writer";
 import {writeEnumClasses} from "./writer/enum-writer";
+import {writeIndexClass} from "./writer/index-writer";
 
 export class ExpressGenerator implements apiscript.Generator {
 
@@ -26,66 +23,10 @@ export class ExpressGenerator implements apiscript.Generator {
         writeEnumClasses(api, libDir);
         writeEntityClasses(api, libDir);
 
-        let writer = new TypescriptWriter(`${libDir}/apiscript.ts`);
-        writer.newLine();
+        // write index class
+        writeIndexClass(api, libDir);
 
-        writer.write('import {Express, Router} from "express";');
-        writer.newLine();
-
-        writer.write('import * as bodyParser from "body-parser";');
-        writer.newLine(2);
-
-        api.forEachEndpoint((endpoint, index) => {
-            let url = transform.urlToDash(endpoint.url);
-
-            writer.write(`import endpoint${index} from '../api/${url}-` +
-                `${apiscript.requestMethodToString(endpoint.requestMethod).toLowerCase()}';`);
-
-            writer.newLine();
-        });
-        writer.newLine();
-
-        writeRequestClasses(api, libDir, writer);
-        writer.newLine();
-
-        writeResponseClasses(api, libDir, writer);
-        writer.newLine();
-
-        writer.write(`export default function (app: express) `);
-        writer.openClosure();
-        writer.newLine();
-
-        writer.indent();
-        writer.write(`let router = new Router();`);
-        writer.newLine(2);
-
-        writer.indent();
-        writer.write(`router.use('/', (request, router) => `);
-        writer.openClosure();
-        writer.newLine();
-
-        writer.indent();
-        writer.write(`response.json({ error: 'Invalid API Endpoint' });`);
-        writer.newLine();
-
-        writer.subIndent();
-        writer.closeClosure();
-        writer.write(`);`);
-        writer.newLine(2);
-
-        writer.indent();
-        writer.write(`app.use(bodyParser.json());`);
-        writer.newLine(1);
-
-        writer.indent();
-        writer.write(`app.use('/${api.name}', router);`);
-        writer.newLine(1);
-
-        writer.closeClosure();
-        writer.close();
-
-        console.log();
-        console.log('Generation complete!');
+        console.log('\nGeneration complete!');
     }
 }
 
--- src/main/writer/index-writer.ts
+++ src/main/writer/index-writer.ts
@@ -1,1 +1,69 @@
 
+import {API} from "apiscript";
+import {TypescriptWriter} from "./typescript-writer";
+import {writeRequestClasses} from "./request-writer";
+import {writeResponseClasses} from "./response-writer";
+
+import * as apiscript from "apiscript";
+import * as transform from "../util/text-transformers";
+
+export function writeIndexClass(api: API, libDir: string) {
+
+    let writer = new TypescriptWriter(`${libDir}/apiscript.ts`);
+    writer.newLine();
+
+    writer.write('import {Express, Router} from "express";');
+    writer.newLine();
+
+    writer.write('import * as bodyParser from "body-parser";');
+    writer.newLine(2);
+
+    api.forEachEndpoint((endpoint, index) => {
+        let url = transform.urlToDash(endpoint.url);
+
+        writer.write(`import endpoint${index} from '../api/${url}-` +
+            `${apiscript.requestMethodToString(endpoint.requestMethod).toLowerCase()}';`);
+
+        writer.newLine();
+    });
+    writer.newLine();
+
+    writeRequestClasses(api, libDir, writer);
+    writer.newLine();
+
+    writeResponseClasses(api, libDir, writer);
+    writer.newLine();
+
+    writer.write(`export default function (app: express) `);
+    writer.openClosure();
+    writer.newLine();
+
+    writer.indent();
+    writer.write(`let router = new Router();`);
+    writer.newLine(2);
+
+    writer.indent();
+    writer.write(`router.use('/', (request, router) => `);
+    writer.openClosure();
+    writer.newLine();
+
+    writer.indent();
+    writer.write(`response.json({ error: 'Invalid API Endpoint' });`);
+    writer.newLine();
+
+    writer.subIndent();
+    writer.closeClosure();
+    writer.write(`);`);
+    writer.newLine(2);
+
+    writer.indent();
+    writer.write(`app.use(bodyParser.json());`);
+    writer.newLine(1);
+
+    writer.indent();
+    writer.write(`app.use('/${api.name}', router);`);
+    writer.newLine(1);
+
+    writer.closeClosure();
+    writer.close();
+}
